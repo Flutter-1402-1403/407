8ebc68c09fd5e6d361d2b629d3c7b994af95d2f7
Nima8FT <nima_8ak@yahoo.com>
2024-03-02 20:39:29 +0330

    finish backend

---
+++ server/app/Http/Controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function store(Request $request)
+    {
+        $msg = "";
+        $users = User::where('email', $request->email)->first();
+        if (!empty($users->id)) {
+            if (Hash::check($request->password, $users->password)) {
+                $msg = "success" . $request->fullname;
+            } else {
+                $msg = "email or password is not correct";
+            }
+        } else {
+            $msg = "not find username or email";
+        }
+
+        return response()->json($msg);
+    }
+}
---
+++ server/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    public function store(Request $request)
+    {
+        $msg = "";
+        $users = User::where('email', $request->email)->orWhere('phone', $request->phone)->first();
+        if (empty($users->id)) {
+            if (preg_match("/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/", $request->email)) {
+                if (preg_match('/^(\+98|0)?9\d{9}$/', $request->phone)) {
+                    User::create([
+                        'fullname' => $request->fullname,
+                        'email' => $request->email,
+                        'password' => $request->password,
+                        'phone' => $request->phone,
+                    ]);
+                    $msg = "success" . $request->fullname;
+                } else {
+                    $msg = "phone number invalid";
+                }
+            } else {
+                $msg = "email does not exist or was misspelled";
+            }
+        } else {
+            $msg = "email or phone number already exist";
+        }
+
+        return response()->json($msg);
+    }
+}
